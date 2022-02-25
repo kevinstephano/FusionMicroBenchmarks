aac1a219240c58ca5f8f1756c7f1fc92df869237
Kevin Stephano <kevin.stephano@gmail.com>
2022-02-25 20:24:36 +0000

    Use context manager to enable NVFuser.

--- Operators/runner.py
+++ Operators/runner.py
@@ -6,15 +6,8 @@ from operator import itemgetter
 import torch
 import torch.nn.functional as F
 
-# Enable NVFuser
-torch._C._jit_set_nvfuser_enabled(True)
-torch._C._jit_set_texpr_fuser_enabled(False)
-torch._C._jit_set_profiling_executor(True)
-torch._C._jit_set_profiling_mode(True)
-torch._C._jit_override_can_fuse_on_cpu(False)
-torch._C._jit_override_can_fuse_on_gpu(False)
+# Enable JIT Autocast
 torch._C._jit_set_autocast_mode(True)
-#torch._C._jit_set_bailout_depth(20)
 
 parser = argparse.ArgumentParser(description='Fusion Benchmark Runner')
 parser.add_argument('--warmup-trials', default=5, type=int, help='Number of trials to not measure.')
@@ -92,51 +85,52 @@ def runner(args, op_modules, tests, reduction_dim=-1) :
                     grads = grads.to(memory_format=torch.channels_last)
  
             # Loop over model implemenatations
-            for impl in op_impls :
-                if impl[0] == 'NVFuser' :
-                    model = torch.jit.script(impl[1](dims[reduction_dim]))
-                else :
-                    model = impl[1](dims[reduction_dim])
-               
-                if args.nhwc :
-                    model = model.to(memory_format=torch.channels_last)
-
-                if args.fp16 :
-                    model.half()
-                model.cuda()
- 
-                elapsed_time_fwd = 0.0
-                elapsed_time_bwd = 0.0
-                for cnt in range(0, args.trials + args.warmup_trials) :
-                    # Setup Step
-                    if not args.inference :
-                        inputs.grad = None
-                        model.zero_grad(set_to_none=True)
-                    clear_l2_cache()
- 
-                    # Time forward
-                    start_evt_fwd.record()
-                    out = model(inputs)
-                    stop_evt_fwd.record()
- 
-                    # Time backward (if enabled)
-                    if not args.inference :
-                        start_evt_bwd.record()
-                        out.backward(grads)
-                        stop_evt_bwd.record()
- 
-                    # Collect timing results
-                    if cnt >= args.warmup_trials :
-                        torch.cuda.synchronize()
-                        elapsed_time_fwd += start_evt_fwd.elapsed_time(stop_evt_fwd)
+            with torch.jit.fuser('fuser2') :
+                for impl in op_impls :
+                    if impl[0] == 'NVFuser' :
+                        model = torch.jit.script(impl[1](dims[reduction_dim]))
+                    else :
+                        model = impl[1](dims[reduction_dim])
+                   
+                    if args.nhwc :
+                        model = model.to(memory_format=torch.channels_last)
+                 
+                    if args.fp16 :
+                        model.half()
+                    model.cuda()
+                 
+                    elapsed_time_fwd = 0.0
+                    elapsed_time_bwd = 0.0
+                    for cnt in range(0, args.trials + args.warmup_trials) :
+                        # Setup Step
                         if not args.inference :
-                            elapsed_time_bwd += start_evt_bwd.elapsed_time(stop_evt_bwd)
-            
-                fwd_time = elapsed_time_fwd / args.trials
-                result += ';' + impl[0] + ';' + str(fwd_time)
- 
-                if not args.inference :
-                    bwd_time = elapsed_time_bwd / args.trials
-                    result += ';' + str(bwd_time)
+                            inputs.grad = None
+                            model.zero_grad(set_to_none=True)
+                        clear_l2_cache()
+                 
+                        # Time forward
+                        start_evt_fwd.record()
+                        out = model(inputs)
+                        stop_evt_fwd.record()
+                 
+                        # Time backward (if enabled)
+                        if not args.inference :
+                            start_evt_bwd.record()
+                            out.backward(grads)
+                            stop_evt_bwd.record()
+                 
+                        # Collect timing results
+                        if cnt >= args.warmup_trials :
+                            torch.cuda.synchronize()
+                            elapsed_time_fwd += start_evt_fwd.elapsed_time(stop_evt_fwd)
+                            if not args.inference :
+                                elapsed_time_bwd += start_evt_bwd.elapsed_time(stop_evt_bwd)
+             
+                    fwd_time = elapsed_time_fwd / args.trials
+                    result += ';' + impl[0] + ';' + str(fwd_time)
+                 
+                    if not args.inference :
+                        bwd_time = elapsed_time_bwd / args.trials
+                        result += ';' + str(bwd_time)
  
             print(result)
